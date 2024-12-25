09fb0d0ddf99281abf1eed4cc35e60015ee66ef0
Mobina Shahbazi <shzmobina294@gmail.com>
2024-12-25 15:31:39 +0330

    Development of limiting access to the login and registration page

--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -1,7 +1,12 @@
 import SigninPage from '@/template/SigninPage'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../api/auth/[...nextauth]/route'
+import { redirect } from 'next/navigation'
 
 
-function SignIn() {
+ async function SignIn() {
+  const session = await getServerSession(authOptions)
+  if (session) redirect("/")
   return (
     <>
     <SigninPage/>
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,7 +1,13 @@
 import SignupPage from "@/template/SignupPage"
 
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../api/auth/[...nextauth]/route'
+import { redirect } from 'next/navigation'
 
-function SignUp() {
+ async function SignUp() {
+  
+  const session = await getServerSession(authOptions)
+  if (session) redirect("/")
   return (
     <>
     <SignupPage/>
