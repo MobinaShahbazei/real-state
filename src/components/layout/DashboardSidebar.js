1f3962d56a6ee076f20e5a4791cb56e25d2e3dfb
Mobina Shahbazi <shzmobina294@gmail.com>
2025-01-09 14:19:51 +0330

    Development of the logout button

--- src/components/layout/DashboardSidebar.js
+++ src/components/layout/DashboardSidebar.js
@@ -3,6 +3,7 @@ import { CgProfile } from "react-icons/cg";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
+import LogoutButton from "@/module/LogoutButton";
 
  async function DashboardSidebar({ children }) {
     const session =await  getServerSession(authOptions)
@@ -10,11 +11,12 @@ import Link from "next/link";
     <div className={styles.container}>
       <div className={styles.sidebar}>
         <CgProfile />
-       <p> {session.user.email}</p>
+       <p> {session?.user.email}</p>
        <span></span>
        <Link href="/dashboard">حساب کاربری</Link>
        <Link href="/dashboard/my-profile">اگهی های من </Link>
        <Link href="/dashboard/add">ثبت اگهی</Link>
+       <LogoutButton/>
       </div>
       <div className={styles.main}>{children}</div>
     </div>
