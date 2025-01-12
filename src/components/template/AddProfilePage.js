0c7bdc8dda92c56d5f8b2f6cb1ab7d8041b976b6
Mobina Shahbazi <shzmobina294@gmail.com>
2025-01-12 15:03:30 +0330

    Sp, p2e, e2p component development and add profile page development

--- src/components/template/AddProfilePage.js
+++ src/components/template/AddProfilePage.js
@@ -1,23 +1,67 @@
-"use client"
-import styles from "@/template/AddProfilePage.module.css"
-import { useState } from "react"
+"use client";
+import TextInput from "@/module/TextInput";
+import styles from "@/template/AddProfilePage.module.css";
+import { useState } from "react";
 
 function AddProfilePage() {
-    const [profileData, setProfileData] = useState({
-        title: "",
-        description: "",
-        location: "",
-        phone: "",
-        price: "",
-        realState: "",
-        constructionDate: new Date(),
-        category: "",
-        rules: [],
-        amenities: [],
-    })
+  const [profileData, setProfileData] = useState({
+    title: "",
+    description: "",
+    location: "",
+    phone: "",
+    price: "",
+    realState: "",
+    constructionDate: new Date(),
+    category: "",
+    rules: [],
+    amenities: [],
+  });
+  const submitHandler = () => {
+    console.log(profileData)
+  }
   return (
-    <div>AddProfilePage</div>
-  )
+    <div className={styles.container}>
+      <h3>ثبت اگهی</h3>
+      <TextInput
+        title="عنوان اگهی"
+        name="title"
+        profileData={profileData}
+        setProfileData={setProfileData}
+      />
+      <TextInput
+        title="توضیحات"
+        name="description"
+        profileData={profileData}
+        setProfileData={setProfileData}
+        textarea={true}
+      />
+      <TextInput
+        title="ادرس"
+        name="location"
+        profileData={profileData}
+        setProfileData={setProfileData}
+      />
+       <TextInput
+        title="شماره تماس"
+        name=" phone"
+        profileData={profileData}
+        setProfileData={setProfileData}
+      />
+       <TextInput
+        title="قیمت(تومان)"
+        name=" price"
+        profileData={profileData}
+        setProfileData={setProfileData}
+      />
+       <TextInput
+        title="بنگاه"
+        name="realState"
+        profileData={profileData}
+        setProfileData={setProfileData}
+      />
+      <button className={styles.submit} onClick={submitHandler}>ثبت اگهی</button>
+    </div>
+  );
 }
 
-export default AddProfilePage
+export default AddProfilePage;
