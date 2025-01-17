eb10ebecd1419c895fc964a9e61d46146ebce580
Mobina Shahbazi <shzmobina294@gmail.com>
2025-01-17 22:47:30 +0330

    Card component development

--- src/components/module/Card.js
+++ src/components/module/Card.js
@@ -1,7 +1,34 @@
+import styles from "@/module/Card.module.css";
+import { RiHome3Line } from "react-icons/ri";
+import { BiLeftArrowAlt } from "react-icons/bi";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { BiStore } from "react-icons/bi";
+import { MdApartment } from "react-icons/md";
+import { GiOfficeChair } from "react-icons/gi";
+import { sp } from "@/utils/replaceNumber";
+import Link from "next/link";
 
-
-function Card({ data }) {
-  return <div>{data.title}</div>;
+function Card({ data: { title, location, price, category } }) {
+  const icons = {
+    villa: <RiHome3Line />,
+    apartment: <MdApartment />,
+    store: <BiStore />,
+    office: <GiOfficeChair />,
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.icon}>{icons[category]}</div>
+      <p className={styles.title}>{title}</p>
+      <p className={styles.location}>
+        {location}
+        <HiOutlineLocationMarker />
+      </p>
+      <span>{sp(price)} تومان</span>
+      <Link href="/">مشاهده اگهی 
+      <BiLeftArrowAlt/>
+      </Link>
+    </div>
+  );
 }
 
 export default Card;
