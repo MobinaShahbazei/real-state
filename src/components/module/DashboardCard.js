3b6172b78ca7abc46091c28ad6cb8558df33d5ff
Mobina Shahbazi <shzmobina294@gmail.com>
2025-01-18 15:39:05 +0330

    Create an api to edit ads

--- src/components/module/DashboardCard.js
+++ src/components/module/DashboardCard.js
@@ -1,9 +1,29 @@
+"use client"
 import Card from "@/module/Card";
+import styles from "@/module/DashboardCard.module.css";
+import { AiOutlineDelete } from "react-icons/ai";
+import { FiEdit } from "react-icons/fi";
 
 function DashboardCard({ data }) {
+    const editHandler = () => {
+
+    }
+    const deleteHandler = () => {
+
+    }
   return (
-    <div>
-      <Card data={data}/>
+    <div className={styles.container}>
+      <Card data={data} />
+      <div className={styles.main}>
+        <button onClick={editHandler}>
+            ویرایش
+           <FiEdit/>
+        </button>
+        <button onClick={deleteHandler}>
+            حذف اگهی
+            <AiOutlineDelete/>
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/template/MyProgilePage.js
+++ src/components/template/MyProgilePage.js
@@ -7,7 +7,7 @@ function MyProgilePage({ profiles }) {
       {profiles.length ? null : (
         <p className={styles.text}>هیچ اگهی ثبت نشده</p>
       )}
-      {profiles.map((i)=> <DashboardCard key={i._id} data={i}/>)}
+      {profiles.map((i)=> <DashboardCard key={i._id} data={JSON.parse(JSON.stringify(i))}/>)}
     </div>
   );
 }
