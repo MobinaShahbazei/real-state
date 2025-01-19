6cefe7d2ef614263bd95c5c133e128c4e45e531f
Mobina Shahbazi <shzmobina294@gmail.com>
2025-01-19 21:00:30 +0330

    Querying and filtering data

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -1,17 +1,22 @@
 import ByResidentialsPage from "@/template/ByResidentialsPage"
 
 
- async function ByResidentials() {
+ async function ByResidentials({searchParams}) {
   const res = await fetch("http://localhost:3000/api/profile", {
     cache: "no-store",
   });
   const data = await res.json();
 
   if (data.error) return <h3>مشکلی پیش آمده است</h3>;
+  let finalData = data.data
+  if(searchParams.category) {
+    finalData = finalData.filter(i => i.category === searchParams.category)
+
+  }
 
   
   return (
-    <ByResidentialsPage data={data.data}/>
+    <ByResidentialsPage data={finalData}/>
   )
 }
 
