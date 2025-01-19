0670a31af958c50b9341ccc325b1b04aac1cf198
Mobina Shahbazi <shzmobina294@gmail.com>
2025-01-19 16:48:33 +0330

    Development of the home page, part 2

--- src/components/template/HomePage.js
+++ src/components/template/HomePage.js
@@ -1,4 +1,5 @@
 import { FiCircle } from "react-icons/fi";
+import { FaCity } from "react-icons/fa";
 import styles from "@/template/HomePage.module.css";
 import CategoryCard from "@/module/CategoryCard";
 
@@ -31,10 +32,20 @@ function HomePage() {
         </div>
       </div>
       <div className={styles.categories}>
-        <CategoryCard title="خانه ویلایی" name="villa"/>
-        <CategoryCard title="اپارتمان" name="apartment"/>
-        <CategoryCard title="مغازه" name="store"/>
-        <CategoryCard title="دفتر" name="office"/>
+        <CategoryCard title="خانه ویلایی" name="villa" />
+        <CategoryCard title="اپارتمان" name="apartment" />
+        <CategoryCard title="مغازه" name="store" />
+        <CategoryCard title="دفتر" name="office" />
+      </div>
+      <div className={styles.city}>
+        <h3>شهر های پر بازدید</h3>
+        <ul>
+            {cities.map((i)=> (<li key={i}>
+                <FaCity />
+              <span>{i}</span>
+          
+            </li>))}
+        </ul>
 
       </div>
     </div>
