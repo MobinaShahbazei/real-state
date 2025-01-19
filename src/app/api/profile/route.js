b6db74516b233225ec2b632b7a48fdee91e9c2aa
Mobina Shahbazi <shzmobina294@gmail.com>
2025-01-19 20:37:08 +0330

    Implement the ads section

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -8,14 +8,17 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     await connectDB();
-    const profiles = await Profile.find().select("-userId");
+
+    const profiles = await Profile.find({ published: true }).select("-userId");
+
     return NextResponse.json(
       {
         data: profiles,
       },
       { status: 200 }
     );
-  } catch (error) {
+  } catch (err) {
+    console.log(err);
     return NextResponse.json(
       { error: "مشکلی در سرور رخ داده است" },
       { status: 500 }
--- src/components/template/ByResidentialsPage.js
+++ src/components/template/ByResidentialsPage.js
@@ -1,11 +1,16 @@
-import styles from "@/template/ByResidentialsPage.module.css"
+import Card from "@/module/Card";
+import styles from "@/template/ByResidentialsPage.module.css";
 
-function ByResidentialsPage() {
+function ByResidentialsPage({ data }) {
   return (
-    <div>
-        
+    <div className={styles.container}>
+      <div className={styles.sidebar}></div>
+      <div className={styles.main}>
+        {data.length ? null : <p className={styles.text}>هیچ اگهی ثبت نشده است</p>}
+        {data.map((profile) => (<Card key={profile._id} data={profile}/>))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default ByResidentialsPage
+export default ByResidentialsPage;
