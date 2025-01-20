3038f63c86618ffbeda467b6daaa14d7eba69dc2
Mobina Shahbazi <shzmobina294@gmail.com>
2025-01-20 14:27:00 +0330

    Sidebar development on the advertisement page

--- src/components/template/ByResidentialsPage.js
+++ src/components/template/ByResidentialsPage.js
@@ -1,10 +1,13 @@
 import Card from "@/module/Card";
+import Sidebar from "@/module/Sidebar";
 import styles from "@/template/ByResidentialsPage.module.css";
 
 function ByResidentialsPage({ data }) {
   return (
     <div className={styles.container}>
-      <div className={styles.sidebar}></div>
+      <div className={styles.sidebar}>
+        <Sidebar/>
+      </div>
       <div className={styles.main}>
         {data.length ? null : <p className={styles.text}>هیچ اگهی ثبت نشده است</p>}
         {data.map((profile) => (<Card key={profile._id} data={profile}/>))}
