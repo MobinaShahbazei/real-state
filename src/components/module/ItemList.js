5a1e5d1edb82d2d4656187966e1e5489f258bc14
Mobina Shahbazi <shzmobina294@gmail.com>
2025-01-20 16:12:22 +0330

    Add texts to the ad page

---
+++ src/components/module/ItemList.js
@@ -0,0 +1,15 @@
+import styles from "@/module/ItemList.module.css"
+
+function ItemList({data}) {
+  return (
+    <div className={styles.container}>
+    {data.length ? (
+            <ul>
+                {data.amenities.map((amenity, index) => <li key={index}>{amenity}</li>)}
+            </ul>
+        ) : (<p>هیچ موردی ذکر نشده است</p>)} 
+    </div>
+  )
+}
+
+export default ItemList
--- src/components/template/DetailsPage.js
+++ src/components/template/DetailsPage.js
@@ -1,9 +1,46 @@
-import styles from "@/template/DetailsPage.module.css"
+import styles from "@/template/DetailsPage.module.css";
+import { SiHomebridge } from "react-icons/si";
+import { AiOutlinePhone } from "react-icons/ai";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { BiCalendarCheck, BiStore } from "react-icons/bi";
+import { RiHome3Line } from "react-icons/ri";
+import { MdApartment } from "react-icons/md";
+import { BiStore } from "react-icons/bi";
+import { GiOfficeChair } from "react-icons/gi";
+import ItemList from "@/module/ItemList";
+import Title from "@/module/Title";
 
-function DetailsPage() {
+function DetailsPage({ data }) {
+  const categories = {
+    apartment: "اپارتمان",
+    villa: "ویلا",
+    store: "مغازه",
+    office: "دفتر",
+  };
+  const icons = {
+    villa: <RiHome3Line />,
+    apartment: <MdApartment />,
+    store: <BiStore />,
+    office: <GiOfficeChair />,
+  };
   return (
-    <div>DetailsPage</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <h1>{data.title}</h1>
+        <span>
+          <HiOutlineLocationMarker />
+          {data.location}
+        </span>
+       <Title>توضیحات</Title>
+        <p>{data.description}</p>
+        <Title>امکانات</Title>
+        <ItemList data={data.amenities}/>
+        <Title>قوانین</Title>
+        <ItemList data={data.rules}/>
+      </div>
+      <div className={styles.sidebar}></div>
+    </div>
+  );
 }
 
-export default DetailsPage
+export default DetailsPage;
